71f6b4d3e7231151332f1a505dc022c8599e134b
Tommi Väyrynen <105491737+tommivayrynen1@users.noreply.github.com>
2023-01-04 15:16:07 +0200

    Add files via upload

---
+++ load_neurone.m
@@ -0,0 +1,34 @@
+%OPEN BINARY NeurOne Brain Vision Data exports (IEEE_float_32) in matlab.
+% input: 'filename.eeg'; output raw,srate
+
+function [raw,srate] = load_eeg(filename)
+
+filename_eeg = string(filename);
+filename_eeg = strsplit(filename_eeg,'.');filename_eeg = filename_eeg{1,1};
+
+    fid = fopen(sprintf('%s.vhdr',filename_eeg));
+    istring = fscanf(fid,'%s');
+    fclose(fid);   
+    dataformat = strsplit(istring,'DataFormat=');
+    dataformat = strsplit(dataformat{1,2},'D');dataformat=string(dataformat{1,1});
+    
+    if dataformat == 'BINARY'
+        chan_lkm = strsplit(istring,'NumberOfChannels=');
+        chan_lkm= strsplit(chan_lkm{1,2},'SamplingInterval');chan_lkm=str2num(chan_lkm{1,1});
+
+        orientation = strsplit(istring,'DataOrientation=');
+        orientation = strsplit(orientation{1,2},'DataType');orientation=string(orientation{1,1});
+
+        samplinginter = strsplit(istring,'SamplingInterval=');
+        samplinginter = strsplit(samplinginter{1,2},'[');samplinginter=str2num(samplinginter{1,1});
+        srate = 1000000 / samplinginter;
+
+         if orientation == 'MULTIPLEXED'
+         a = fopen(sprintf('%s.eeg',filename_eeg));
+         raw = fread(a,[chan_lkm inf],'float32');
+         raw = raw.*0.001; % convert units to microvolts. In the header units are in 0.001muV
+         fclose(a);
+         end
+    end
+
+end
